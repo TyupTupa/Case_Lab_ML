f1e88d3a3e99f5012bcf3ce8af266fbff4cf224e
TyupTupa <marysik1596@gmail.com>
2024-10-15 23:18:18 +0300

    Delete static

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', include('main.urls')),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
